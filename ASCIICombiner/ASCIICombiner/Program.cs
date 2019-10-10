87e56002d58d03cb7fc4e10015c426e3949c98eb
renestadler <stadler.rene2000@gmail.com>
2019-10-10 11:50:58 +0200

    Final Version

---
+++ ASCIICombiner/ASCIICombiner/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace ASCIICombiner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length < 2)
+            {
+                Console.WriteLine("Not enough arguments specified");
+                Console.WriteLine("Usage: ASCIIArtCombiner.exe <textfile1> <textfile2>");
+            }
+            ProcessFiles(args);
+        }
+
+        private async static void ProcessFiles(string[] files)
+        {
+            var texts = new List<string[]>();
+
+            for (int i = 0; i < files.Length; i++)
+            {
+                texts.Add((await File.ReadAllTextAsync(files[i])).Replace("\r", string.Empty).Split("\n"));
+            }
+            Console.WriteLine((await File.ReadAllTextAsync(files[0])).Replace("\r", string.Empty).Split("\n"));
+            Console.WriteLine(texts[1]);
+            FileMerger fm = new FileMerger();
+            char[][] ar = fm.mergeFiles(texts);
+            if (ar == null)
+            {
+                Console.WriteLine("Files are not all of equal length");
+                Console.WriteLine("Usage: ASCIIArtCombiner.exe <textfile1> <textfile2>");
+                return;
+            }
+            for (int i = 0; i < ar.Length; i++)
+            {
+                Console.WriteLine(new string(ar[0]));
+            }
+        }
+    }
+}
---
+++ ASCIICombiner/ASCIICombinerLibrary/FileMerger.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+
+namespace ASCIICombiner
+{
+    public class FileMerger
+    {
+        public char[][] mergeFiles(System.Collections.Generic.List<string[]> args)
+        {
+            char[][] ar = new char[args.First().Length][];
+            for (int i = 0; i < ar.Length; i++)
+            {
+                ar[i] = new char[args.First()[0].Length];
+            }
+            for (int i = 0; i < args.First().Length; i++)
+            {
+                if (args.First().Length != ar.Length)
+                {
+                    return null;
+                }
+                for (int j = 0; j < args.First()[0].Length; j++)
+                {
+                    if (args.First()[0].Length != ar.Length)
+                    {
+                        return null;
+                    }
+                    for (int k = 0; k < args.Count(); k++)
+                    {
+                        if (args[k][i][j] != ' ')
+                        {
+                            ar[i][j] = args[k][i][j];
+                        }
+                    }
+                }
+            }
+            return ar;
+        }
+    }
+}
